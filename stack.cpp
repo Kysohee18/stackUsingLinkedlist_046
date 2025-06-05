9caf848db47110875ad7d55b391a91fa3f00e260
Kyosohee18 <hanashahky@gmail.com>
2025-06-05 10:32:28 +0700

    membuat class node

--- stack.cpp
+++ stack.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+//node class representing a single node in the linked list
+class Node
+{
+    public: 
+    int data;
+    Node *next;
+
+    node()
+    {
+        next = NULL;
+
+    }
+};
+
+
