e90aa5e758bff2cd50eda9c539712b49fc676808
Kyosohee18 <hanashahky@gmail.com>
2025-06-05 11:11:42 +0700

    int push

--- stack.cpp
+++ stack.cpp
@@ -24,5 +24,18 @@ class stack
     {
         top = NULL; // initialize the stack with a a null top pointer 
     }
+    // push operation: insert a element onto the top of the stack 
+    int push (int value)
+    {
+        Node *newNode = new Node();// 1.allocate memory for the new node
+        newNode->data = value; // 2.assingn value
+        newNode->next =top; // set the next pointer of the new node to the current top 
+        top = newNode; // update the top pointer to the new node
+        cout << "push value: " << value << endl;
+        return value;
+    }
+
+ 
+
 
 
